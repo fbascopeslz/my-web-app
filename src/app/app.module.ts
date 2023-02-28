aa77a221aad2fb680bab960d70f2f990d98d8b8a
fbascopeslz <fbascopeslz@gmail.com>
2023-02-28 15:42:50 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user.component';
+import { BooksComponent } from './books/books.component';
+import { BookComponent } from './book/book.component';
+import { FormsModule } from '@angular/forms';
+import { BooksService } from './services/books.service';
+import { StartComponent } from './start.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    BooksComponent,
+    BookComponent,
+    StartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    BooksService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
